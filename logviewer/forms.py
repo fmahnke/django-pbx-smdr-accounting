473b8cf35a9599a555f0141d70010c92a73babda
Fritz Mahnke <fritz@fritzmahnke.com>
2012-06-04 16:59:53 -0500

    Work on search form

--- logviewer/forms.py
+++ logviewer/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class RecordSearchForm(forms.Form):
     start_date = forms.DateField()
     end_date   = forms.DateField()
-    incoming   = forms.BooleanField()
-    outgoing   = forms.BooleanField()
+    incoming   = forms.BooleanField(required=False, initial=True)
+    outgoing   = forms.BooleanField(required=False, initial=True)
