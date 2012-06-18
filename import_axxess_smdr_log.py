86bcb5e2d5fa42987d806057b1f8b6740aaab802
Fritz Mahnke <fritz@fritzmahnke.com>
2012-06-18 17:00:31 -0500

    Correct import script to get ring without trailing spaces

--- import_axxess_smdr_log.py
+++ import_axxess_smdr_log.py
@@ -4,6 +4,8 @@ import re
 import sqlite3
 import sys
 
+DATABASE_NAME = 'sqdatabase.db'
+
 if (len(sys.argv) > 1):
 	fileWithPath = sys.argv[1];
 else:
@@ -11,72 +13,71 @@ else:
 	sys.exit(1)
 
 fileName = basename(fileWithPath)
+print "Reading file: " + fileName
 m        = re.match(r'phone_log_(\d+?)-(\d+?)-(\d+?)\.log', fileName)
 logYear  = int(m.group(1))
 logMonth = int(m.group(2))
 logDay   = int(m.group(3))
 
-conn = sqlite3.connect('sqdatabase')
+conn = sqlite3.connect(DATABASE_NAME)
 c    = conn.cursor()
 
 f    = open(fileWithPath)
 
 for line in f:
-	# Empty line?
-	if (line == '\n'):
-		continue
-	m = re.match(r'Station', line)
-	# Footer?
-	if (m is not None):
-		continue
-	m = re.match(r'TYP', line)
-	# Other footer?
-	if (m is not None):
-		continue
+    # Empty line?
+    if (line == '\n'):
+        continue
+    m = re.match(r'Station', line)
+# Footer?
+    if (m is not None):
+        continue
+    m = re.match(r'TYP', line)
+# Other footer?
+    if (m is not None):
+        continue
 
-	m = re.match(r'([\w\s/]+?)([\d\*]+?)\s+?(\d{5})\s+?[RING\s\.]*?([\d\-]+?)\s+?(\d*?)\s+?(\d\d):(\d\d)\s+?S=(\d+?)\s', line)
-	print "Line: " + line
-	type = m.group(1)
-	if (re.match(r'\s', type) is not None):
-		type = 'RG'
-	
-	if (m.group(2) == '*****'):
-		ext = 0
-	else:
-		ext = int(m.group(2))
+    m = re.match(r'([\w/]*?)\s+?([\d\*]+?)\s+?(\d{5})\s+?[RING\s\.]*?([\d\-]+?)\s+?(\d*?)\s+?(\d\d):(\d\d)\s+?S=(\d+?)\s', line)
+    print "Line: " + line
+    # Type field returned an empty string?
+    if (not m.group(1)):
+        type = 'RG'
+    else:
+        type = m.group(1)
 
-	trunk = int(m.group(3))
+    if (m.group(2) == '*****'):
+        ext = 0
+    else:
+        ext = int(m.group(2))
 
-	# Strip hyphens from phone number
-	phone_number = int(m.group(4).replace('-', ''))
+    trunk = int(m.group(3))
 
-	if (m.group(5) is not ''):
-		incoming_ext = int(m.group(5))
-	else:
-		incoming_ext = 0
+# Strip hyphens from phone number
+    phone_number = int(m.group(4).replace('-', ''))
 
-	start_time_hh = int(m.group(6))
-	start_time_mm = int(m.group(7))
-	duration = int(m.group(8))
+    if (m.group(5) is not ''):
+        incoming_ext = int(m.group(5))
+    else:
+        incoming_ext = 0
 
-	start_time = datetime.datetime.combine(
-		datetime.date(logYear, logMonth, logDay),
-		datetime.time(hour=start_time_hh, minute=start_time_mm))
+    start_time_hh = int(m.group(6))
+    start_time_mm = int(m.group(7))
+    duration = int(m.group(8))
 
-	# Print record to console for debugging
-	print "Type: "             + type
-	print "Ext: "              + str(ext)
-	print "Trunk: "            + str(trunk)
-	print "Number and stuff: " + str(phone_number)
-	#print "Incoming ext: "    + str(incoming_ext)
-	print "Start time: "       + str(start_time)
-	print "Duration: "         + str(duration)
+    start_time = datetime.datetime.combine(
+        datetime.date(logYear, logMonth, logDay),
+        datetime.time(hour=start_time_hh, minute=start_time_mm))
 
-	c.execute(
-		"INSERT INTO logviewer_phonerecord
-		 VALUES (NULL,'%s',%d,%d,%d,%d,%d,'%s','%s')"
-		% (type, ext, trunk, 0, phone_number, incoming_ext, start_time,
-			datetime.timedelta(seconds=duration)))
+# Print record to console for debugging
+    print "Type: "             + type
+    print "Ext: "              + str(ext)
+    print "Trunk: "            + str(trunk)
+    print "Number and stuff: " + str(phone_number)
+#print "Incoming ext: "    + str(incoming_ext)
+    print "Start time: "       + str(start_time)
+    print "Duration: "         + str(duration)
+
+    c.execute("INSERT INTO logviewer_phonerecord VALUES (NULL,'%s',%d,%d,%d,%d,'%s','%s')" % (type, ext, trunk, phone_number, incoming_ext, start_time, datetime.timedelta(seconds=duration)))
 
 f.close()
 
